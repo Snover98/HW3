76887a74b4f484467e0ccd08b3c64d30e18b6a33
Assaf-Haik-Barouch <Assaf98@gmail.com>
2018-12-12 09:43:53 +0200

    added parent pointer so that a scope can use it's parent scope added vector for the scope's structure types fixed calculation of offset in addEntry func

--- SymTable.cpp
+++ SymTable.cpp
@@ -3,19 +3,34 @@
 //
 
 #include "SymTable.h"
+#define CALCOFFSET() scope_entries.empty() ? table_offset : scope_entries.back().offset + typeOffset(scope_entries.back())
+
+int SymTable::typeOffset(SymEntry entry){
+    int offset = 0;
+
+    switch(entry.type){
+        case STRUCTTYPE:
+            offset += structTypeOffset(entry.struct_type)-1;
+        default:
+            offset++;
+        case FUNCTYPE:
+            ;
+    }
+
+    return offset;
+}
 
 SymEntry SymTable::getSymbolEntry(std::string ID) {
     //find the entry with the same ID in this table
-    for(std::vector<SymEntry>::iterator it = entries.begin(); it != entries.end(); ++it){
-        if(ID.compare((*it).ID) == 0){
+    for (std::vector<SymEntry>::iterator it = scope_entries.begin(); it != scope_entries.end(); ++it) {
+        if (ID.compare((*it).ID) == 0) {
             return (*it);
         }
     }
 
-    //if there is non, return an entry with type of NOTYPE
-    return SymEntry();
+    //if there is non, try the parent table, and if this table has no parent return an entry with type of NOTYPE
+    return (parent == NULL) ? SymEntry() : parent->getSymbolEntry(ID);
 }
-
 
 
 varType SymTable::getSymType(std::string ID) {
@@ -30,22 +45,42 @@ FunctionType SymTable::getFunctionType(std::string ID) {
     return getSymbolEntry(ID).func_type;
 }
 
-std::string SymTable::getStructType(std::string ID){
+std::string SymTable::getStructType(std::string ID) {
     return getSymbolEntry(ID).struct_type;
 }
 
-void SymTable::addEntry(SymEntry e){
-    entries.push_back(e);
+void SymTable::addEntry(SymEntry e) {
+    if(isSymInTable(e.ID)){
+        throw ShadowingException(e.ID);
+    }
+
+    scope_entries.push_back(e);
 }
 
-void SymTable::addEntry(std::string ID, std::string struct_type, int offset){
-    addEntry(SymEntry(ID,struct_type, offset + entries.empty() ? table_offset : entries.back().offset));
+void SymTable::addEntry(std::string ID, std::string struct_type) {
+    addEntry(SymEntry(ID, struct_type, CALCOFFSET()));
 }
 
-void SymTable::addEntry(std::string ID, varType type){
-    addEntry(SymEntry(ID,type,1 + entries.empty() ? table_offset : entries.back().offset));
+void SymTable::addEntry(std::string ID, varType type) {
+    addEntry(SymEntry(ID, type, CALCOFFSET()));
 }
 
-void SymTable::addEntry(std::string ID, std::vector<varType> func_params, varType ret_type){
-    addEntry(SymEntry(ID, entries.empty() ? table_offset : entries.back().offset, func_params, ret_type));
+void SymTable::addEntry(std::string ID, std::vector<varType> func_params, varType ret_type) {
+    addEntry(SymEntry(ID, CALCOFFSET(), func_params, ret_type));
+}
+
+StructType SymTable::getStructTypeEntry(std::string ID){
+    //find the struct type with the same ID in this table
+    for (std::vector<StructType>::iterator it = scope_structs.begin(); it != scope_structs.end(); ++it) {
+        if (ID.compare((*it).type_name) == 0) {
+            return (*it);
+        }
+    }
+
+    //if there is non, try the parent table, and if this table has no parent return an entry with type of NOTYPE
+    return (parent == NULL) ? StructType() : parent->getStructTypeEntry(ID);
+}
+
+int SymTable::structTypeOffset(std::string ID){
+    return getStructTypeEntry(ID).fields.size();
 }
--- SymTable.h
+++ SymTable.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <vector>
+#include "Exceptions.h"
 
 enum varType {
     INTTYPE, BYTETYPE, BOOLTYPE, STRUCTTYPE, FUNCTYPE, NOTYPE
@@ -14,43 +15,65 @@ enum varType {
 
 typedef std::pair<std::vector<varType>, varType> FunctionType;
 
+struct StructType {
+    const std::string type_name;
+    const std::vector<std::pair<std::string, varType> > fields;
+
+    StructType() : type_name(""), fields(std::vector<std::pair<std::string, varType> >()){}
+
+    StructType(StructType &t) : type_name(std::string(t.type_name)), fields(t.fields) {}
+
+    StructType(const std::string &name, const std::vector<std::pair<std::string, varType> > &fields)
+            : type_name(std::string(name)), fields(std::vector<std::pair<std::string, varType> >(fields)) {}
+};
+
 
 struct SymEntry {
-    std::string ID;
-    varType type;
-    int offset;
+    const std::string ID;
+    const varType type;
+    const int offset;
     FunctionType func_type;
     std::string struct_type;
 
     SymEntry() : type(NOTYPE), offset(0) {}
 
-    SymEntry(std::string ID, std::string struct_type, int offset) : ID(std::string(ID)), type(STRUCTTYPE),
-                                                                    offset(offset),
-                                                                    struct_type(std::string(struct_type)) {}
+    SymEntry(const std::string &ID, const std::string &struct_type, int offset) : ID(std::string(ID)), type(STRUCTTYPE),
+                                                                                  offset(offset),
+                                                                                  struct_type(
+                                                                                          std::string(struct_type)) {}
 
-    SymEntry(std::string ID, varType type, int offset) : ID(std::string(ID)), type(type), offset(offset) {}
+    SymEntry(const std::string &ID, varType type, int offset) : ID(std::string(ID)), type(type), offset(offset) {}
 
-    SymEntry(std::string ID, int offset, std::vector<varType> func_params, varType ret_type) : ID(std::string(ID)),
-                                                                                               type(FUNCTYPE),
-                                                                                               offset(offset),
-                                                                                               func_type(FunctionType(
-                                                                                                       func_params,
-                                                                                                       ret_type)) {}
-
+    SymEntry(const std::string &ID, int offset, const std::vector<varType> &func_params, varType ret_type) : ID(
+            std::string(ID)),
+                                                                                                             type(FUNCTYPE),
+                                                                                                             offset(offset),
+                                                                                                             func_type(
+                                                                                                                     FunctionType(
+                                                                                                                             func_params,
+                                                                                                                             ret_type)) {}
 };
 
 class SymTable {
 private:
     int table_offset;
-    std::vector<SymEntry> entries;
+    std::vector<SymEntry> scope_entries;
+    std::vector<StructType> scope_structs;
+    SymTable *parent;
+
+    int typeOffset(SymEntry entry);
 
 
 public:
-    SymTable(int offset) : table_offset(offset), entries(std::vector<SymEntry>()) {}
+    SymTable(int offset, SymTable *parent = NULL) : table_offset(offset), scope_entries(std::vector<SymEntry>()),
+                                                    parent(parent) {}
 
     void addEntry(SymEntry e);
-    void addEntry(std::string ID, std::string struct_type, int offset);
+
+    void addEntry(std::string ID, std::string struct_type);
+
     void addEntry(std::string ID, varType type);
+
     void addEntry(std::string ID, std::vector<varType> func_params, varType ret_type);
 
     SymEntry getSymbolEntry(std::string ID);
@@ -63,6 +86,9 @@ public:
 
     std::string getStructType(std::string ID);
 
+    StructType getStructTypeEntry(std::string ID);
+
+    int structTypeOffset(std::string ID);
 };
 
 
